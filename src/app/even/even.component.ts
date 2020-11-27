96bc5899f3e0d01d3cda1ab97c600439272b2ffc
Alexander-Giammaruti <argiammaruti@gmail.com>
2020-11-27 12:02:53 -0600

    Added assignment details to the assignment for clarity

--- src/app/even/even.component.ts
+++ src/app/even/even.component.ts
@@ -7,7 +7,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class EvenComponent implements OnInit {
   @Input() number: number;
-
   constructor() { }
 
   ngOnInit(): void {
